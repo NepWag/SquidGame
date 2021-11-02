793ff1f3b0d163cecf54449a797797c02814e97c
Sharosez <59418116+NepWag@users.noreply.github.com>
2021-11-02 09:51:14 +0545

    Level 50 added successful

---
+++ Assets/Scripts/BoxUpDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxUpDown : MonoBehaviour
+{
+    private bool UpDown = true;
+    void Update()
+    {
+        if (UpDown)
+             transform.Translate(0, Time.deltaTime * 0.3f, 0, Space.World);
+        else
+             transform.Translate(0, -Time.deltaTime * 0.3f, 0, Space.World);
+        if(transform.position.y >= -2.33f) 
+        {
+             UpDown = false;
+        }         
+        if(transform.position.y <= -4.5) 
+        {
+             UpDown = true;
+        }  
+    }
+}
